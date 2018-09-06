9b93cea6a841238465444850f6fb1308c7891a2a
sperrin <sperrin@iiu.co.jp>
2018-09-06 15:31:28 +0900

    New commands system Updated readme.md

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
 # SEALC
-System for Electro-Acoustic Live Coding
+# System for Electro-Acoustic Live Coding
 
+# Description
 Microcontroller program to live code motors (steppers and servos) through a serial terminal.
 
-[motor#]command[argument]
+[motor# (0-9)]command[argument]
+Separate several commands by comma to send them at once.
+No need to re-selected motor once selected.
+
+# Commands:
+SS set speed:
+argument in RPM
+
+SD set direction:
+argument=0 => CW
+argument>0 => CCW
+
+ST stop motion:
+no argument
+
+RO rotate:
+no argument or argument=0 => continuously
+argument>0 => arument is number of turns
+
+RW rotate wave:
+rotate continuously with triangle speed variation (0-speed). Argument is number of periods per turn.
+
+RA rotate angle:
+argument is angle in degrees.
+
+SQ sequence:
+argument is [angle:(0-2):...:(0-2)]
+sweep-like motion of angle. 0 means no motion, 1 is motion in current direction, 2 is motion in opposite current direction.
+
 
--- SEALC.ino
+++ SEALC.ino
@@ -22,32 +22,28 @@
 // pins
 #define EN 8 // stepper motor enable, low level effective
 // serial commands
-#define COMMAND_SPEED 's'
-#define COMMAND_DIR 'd'
-#define COMMAND_ROTATE 'r'
-#define COMMAND_STOP 'x'
-#define COMMAND_MOVE 'm'
-#define COMMAND_WAVE 'w'
-#define COMMAND_BEAT 'b'
 #define SEPARATOR ','
 #define EOL '\n'
 #define COLUMN ':'
 // commands
-#define C_COMMAND_SELECT 0
-#define C_COMMAND_SPEED 1
-#define C_COMMAND_DIR 2
-#define C_COMMAND_ROTATE 3
-#define C_COMMAND_STOP 4
-#define C_COMMAND_MOVE 5
-#define C_COMMAND_NONE 6
-#define C_COMMAND_WAVE 7
-#define C_COMMAND_BEAT 8
+#define COMMAND_SELECT 0
+#define COMMAND_SPEED 1
+#define COMMAND_DIR 2
+#define COMMAND_ROTATE 3
+#define COMMAND_STOP 4
+#define COMMAND_MOVE 5
+#define COMMAND_NONE 6
+#define COMMAND_WAVE 7
+#define COMMAND_BEAT 8
+#define COMMAND_ERROR 9
 
 // vars
 Motor *motors[N_MOTORS];
 int selectedMotor;
 int currentCommand, currentValue;
 bool firstChar = true;
+char command[2];
+int iCommand;
 
 void displaySelectedMotor()
 {
@@ -72,6 +68,117 @@ void updateValue(char a)
   currentValue += (a - 48);
 }
 
+void processCommand()
+{
+  if (iCommand == 1)
+  {
+    switch (command[0])
+    {
+    case COLUMN:
+      if (firstChar)
+      {
+        currentCommand = COMMAND_NONE;
+      }
+      motors[selectedMotor]->setInterBeat(currentValue);
+      currentValue = -1;
+      iCommand = 0;
+      command[0] = 0;
+      break;
+    case SEPARATOR:
+    case EOL:
+      if (firstChar)
+      {
+        currentCommand = COMMAND_NONE;
+      }
+      switch (currentCommand)
+      {
+      case COMMAND_SPEED:
+        motors[selectedMotor]->setSpeed(currentValue);
+        break;
+      case COMMAND_DIR:
+        motors[selectedMotor]->setDir(currentValue);
+        break;
+      case COMMAND_ROTATE:
+        motors[selectedMotor]->setRotate(currentValue);
+        break;
+      case COMMAND_STOP:
+        motors[selectedMotor]->setNextMode(MODE_STOP);
+        motors[selectedMotor]->stop();
+        break;
+      case COMMAND_MOVE:
+        motors[selectedMotor]->setMove(currentValue);
+        break;
+      case COMMAND_WAVE:
+        motors[selectedMotor]->setWave(currentValue);
+        break;
+      case COMMAND_BEAT:
+        motors[selectedMotor]->initBeat();
+        motors[selectedMotor]->setBeat(currentValue);
+        break;
+      case COMMAND_SELECT:
+      case COMMAND_NONE:
+        break;
+      }
+      currentValue = -1;
+      firstChar = true;
+      iCommand = 0;
+      command[0] = 0;
+      break;
+    }
+  }
+  else if (iCommand == 2)
+  {
+    switch (command[0])
+    {
+    case 's':
+    case 'S':
+      switch (command[1])
+      {
+      case 's':
+      case 'S':
+        currentCommand = COMMAND_SPEED;
+        break;
+      case 'd':
+      case 'D':
+        currentCommand = COMMAND_DIR;
+        break;
+      case 't':
+      case 'T':
+        currentCommand = COMMAND_STOP;
+        break;
+      case 'q':
+      case 'Q':
+        currentCommand = COMMAND_BEAT;
+        break;
+      }
+      break;
+    case 'r':
+    case 'R':
+      switch (command[1])
+      {
+      case 'o':
+      case 'O':
+        currentCommand = COMMAND_ROTATE;
+        break;
+      case 'W':
+      case 'w':
+        currentCommand = COMMAND_WAVE;
+        break;
+      case 'a':
+      case 'A':
+        currentCommand = COMMAND_MOVE;
+        break;
+      }
+      break;
+    }
+    currentValue = -1;
+    firstChar = false;
+    iCommand = 0;
+    command[0] = 0;
+    command[1] = 0;
+  }
+}
+
 void setup()
 {
   motors[0] = new Stepper(48, 2, 5);
@@ -83,8 +190,11 @@ void setup()
   displaySelectedMotor();
   pinMode(EN, OUTPUT);
   digitalWrite(EN, LOW);
-  currentValue = 0;
-  currentCommand = C_COMMAND_NONE;
+  currentValue = -1;
+  currentCommand = COMMAND_NONE;
+  command[0] = 0;
+  command[1] = 0;
+  iCommand = 0;
 }
 
 void loop()
@@ -97,7 +207,7 @@ void loop()
     {
       if (firstChar)
       {
-        currentCommand = C_COMMAND_SELECT;
+        currentCommand = COMMAND_SELECT;
         selectedMotor = a - 48;
         if (selectedMotor >= N_MOTORS)
           selectedMotor = N_MOTORS - 1;
@@ -111,39 +221,11 @@ void loop()
     }
     else
     {
-      switch (a)
-      {
-      case COMMAND_ROTATE:
-        if (firstChar)
-          firstChar = false;
-        currentCommand = C_COMMAND_ROTATE;
-        break;
-      case COMMAND_STOP:
-        if (firstChar)
-          firstChar = false;
-        currentCommand = C_COMMAND_STOP;
-        break;
-      case COMMAND_SPEED:
-        if (firstChar)
-          firstChar = false;
-        currentCommand = C_COMMAND_SPEED;
-        break;
-      case COMMAND_DIR:
-        if (firstChar)
-          firstChar = false;
-        currentCommand = C_COMMAND_DIR;
-        break;
-      case COMMAND_MOVE:
-        if (firstChar)
-          firstChar = false;
-        currentCommand = C_COMMAND_MOVE;
-        break;
-      case COMMAND_WAVE:
-        if (firstChar)
-          firstChar = false;
-        currentCommand = C_COMMAND_WAVE;
-        break;
-      case COMMAND_BEAT:
+      command[iCommand++] = a;
+    }
+    processCommand();
+  }
+  /*case COMMAND_BEAT:
         if (firstChar)
           firstChar = false;
         currentCommand = C_COMMAND_BEAT;
@@ -196,8 +278,7 @@ void loop()
         firstChar = true;
         break;
       }
-    }
-  }
+    }*/
   for (int i = 0; i < N_MOTORS; i++)
   {
     motors[i]->action();
